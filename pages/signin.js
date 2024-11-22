46d4100412dd29c7011646214e4d0c2f5e5af3ba
nceplayindia <niceplayindia@gmail.com>
2024-11-22 16:54:01 +0530

    Initial commit - adapt to production

--- pages/signin.js
+++ pages/signin.js
@@ -5,7 +5,7 @@ export default function SignInPage() {
 
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>LooperCode</h1>
+      <h1>Loopercode</h1>
       {session ? (
         <div>
           <p>Signed in as {session.user.name}</p>
